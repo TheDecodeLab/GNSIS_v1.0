3478784079f9e35b4de0c7b97695b6d5b21b3dfa
Venkatesh Avula <42148392+venkateshavula@users.noreply.github.com>
2020-07-21 13:09:33 -0400

    Update featureSelection.R

--- ModelingRecurrentStroke/src/featureSelection.R
+++ ModelingRecurrentStroke/src/featureSelection.R
@@ -50,3 +50,39 @@ corr<-cor(trainSet.imp %>%
                    "DAYS_BTW_LASTOP_INDEX",
                    "CREATININE_CLOSEST_TO_INDEX"),
           method = "pearson")
+# Univariate Filters
+sbfWithFilter<-sbf(RECUR_STROKE~.,
+                   data=trainSet.imp %>% 
+                          select(-c("PT_ID")),
+                   sbfControl=sbfControl(functions=caretSBF,method="repeatedcv",repeats=10))
+sbfWithFilter["optVariables"]
+##:::::::::::::::::
+## Wrapper Methods
+##::::::::::::::::
+# Boruta
+library(Boruta)
+library(dplyr)
+boruta_output <- Boruta(RECUR_STROKE~.,
+                        data=trainSet.imp %>% 
+                          select(-c("PT_ID")),
+                        doTrace=1)
+getSelectedAttributes(boruta_output,withTentative = TRUE)
+TentativeRoughFix(boruta_output)
+imps <- attStats(TentativeRoughFix(boruta_output))
+imps2 <- imps[imps$decision !='Rejected',c('meanImp','decision')]
+imps2[order(-imps2$meanImp),]
+# Recursive feature elimination
+caretFuncs$summary <- twoClassSummary
+control<-rfeControl(functions=caretFuncs,
+                    method = "repeatedcv",
+                    repeats = 10,
+                    verbose = F) 
+rfeProfile<-rfe(x,y,
+                rfeControl = control,
+                method = "svmRadial",
+                metric = "ROC",
+                trControl = trainControl(classProbs = T,
+                                         summaryFunction = twoClassSummary))
+rfeProfile
+rfeProfile$fit
+predictors(rfeProfile)
