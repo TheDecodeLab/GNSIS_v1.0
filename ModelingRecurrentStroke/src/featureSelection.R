d1cd36f0374e8a06d009d6ecdfdec20b8e8398fd
Venkatesh Avula <42148392+venkateshavula@users.noreply.github.com>
2020-07-21 12:47:26 -0400

    Update featureSelection.R

--- ModelingRecurrentStroke/src/featureSelection.R
+++ ModelingRecurrentStroke/src/featureSelection.R
@@ -2,22 +2,16 @@
 ## Built-in Feature Importance
 ##::::::::::::::::::::::::::::
 # Using RandomForest
-y<-as.factor(as.matrix(trainSet.imp %>% 
-                         rename(label="RECUR_STROKE") %>%
-                         mutate(label=factor(label,
-                                             labels=make.names(levels(as.factor(as.character(label)))))) %>%
-                         select(label)))
-x<-trainSet.imp
-x$RECUR_STROKE<-as.factor(as.matrix(trainSet.imp %>% 
+trainSet.imp$RECUR_STROKE<-as.factor(as.matrix(trainSet.imp %>% 
                                          rename(label="RECUR_STROKE") %>%
                                          mutate(label=factor(label,
                                                              labels=make.names(levels(as.factor(as.character(label)))))) %>% 
                                          select(label)))
 builtInRF<-train(RECUR_STROKE~.,
-                 data=x %>% 
-                   select(-c("PT_ID")),
-                 method="rf",
-                 metric="ROC",
+                 data = trainSet.imp %>% 
+                          select(-c("PT_ID")),
+                 method = "rf",
+                 metric = "ROC",
                  trControl = trainControl(classProbs = T,
                                           summaryFunction = twoClassSummary))
 varImp(builtInRF)
